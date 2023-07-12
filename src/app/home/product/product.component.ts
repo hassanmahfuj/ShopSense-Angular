785543c777d5f98c7050b732ad33dd5a5cf494c2
Hassan Mahfuj <itsmahfuj@gmail.com>
2023-07-12 11:44:13 +0600

    complete view product

--- src/app/home/product/product.component.ts
+++ src/app/home/product/product.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Product } from 'src/app/interfaces/product';
+import { CustomerService } from 'src/app/services/customer.service';
 
 @Component({
   selector: 'app-product',
   templateUrl: './product.component.html',
   styleUrls: ['./product.component.css']
 })
-export class ProductComponent {
+export class ProductComponent implements OnInit {
+  product: any;
+
+  constructor(private customerService: CustomerService, private route: ActivatedRoute) { }
+
+  ngOnInit(): void {
+    let id = this.route.snapshot.params['id'];
+    this.customerService.getProduct(id).subscribe((response) => {
+      this.product = response;
+    });
+  }
 
 }
